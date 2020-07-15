8b96231f4582bbc52c1e0054c1e6010c11b8836f
ryums12 <nbfaker@naver.com>
2020-07-15 23:56:10 +0900

    fix test

--- Node.js-board/node_modules/.bin/exd.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\express-g\bin\exd.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\express-g\bin\exd.js" %*
-)
