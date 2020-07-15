2d1d027a3aa6fda23c71c943c93347b3b0b4ec30
ryums12 <nbfaker@naver.com>
2020-07-15 23:48:47 +0900

    git ignore test

---
+++ react-user_manager/src/components/Customer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class customer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>류민송</h2>
+                <p>010712</p>
+                <p>남자</p>
+                <p>사원</p>
+            </div>
+        );
+    }
+}
+
+export default customer;
